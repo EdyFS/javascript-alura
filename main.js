6aacb8f8d83e17b4a2a073cd2ea343e14133c0f6
EdyFS <edy.ferreira.santana@gmail.com>
2025-01-17 19:39:10 -0300

    Uso do teclado

--- main.js
+++ main.js
@@ -24,8 +24,18 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++){
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;//template string
     //console.log(instrumento);
-    listaDeTeclas[contador].onclick = function (){
+    //listaDeTeclas[contador].onclick = function (){
+    tecla.onclick = function (){
         tocaSom(idAudio);     
+    }
+    tecla.onkeydown = function (evento){
+        console.log(evento.code);
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
     }
     
     //contador = contador + 1;
